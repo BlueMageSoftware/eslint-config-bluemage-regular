907b934364ad1f765c974ce8c976cb53129cd475
Noris Alsaadie <noris@bluemagesoftware.com>
2016-09-27 22:15:25 +0100

    ESLint draft

---
+++ airbnb-overrides.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "browser": false
+  },
+  "rules": {
+    "comma-dangle": ["error", "never"],
+    "semi": ["error", "never"]
+  }
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "browser": false
+  },
+  "extends": [
+    "./airbnb-overrides.js"
+  ],
+  "rules": {}
+}
